678cbf6fe75dfd52eef924a7b8be33f71625f370
tokaismail1 <tokaismail1122@gmail.com>
2025-03-09 18:50:59 +0200

    task2

--- Assignment/script.js
+++ Assignment/script.js
@@ -1,1 +1,40 @@
-// Write the codes in here
+//declares variables
+var myAge =21;
+
+let myLevel = 3;
+
+const myName = "Toka";
+
+//arithmetic and logical operators.
+
+let x = 6;
+let y = 5;
+let z = 4;
+//arithmetic
+let sum = x + y ;
+let product = x * y;
+let subtraction = x - y;
+let division = x / y;
+let mod = x % y;
+
+console.log(sum , product , subtraction , division , mod)
+
+//logical
+let and = x>y && y>z;
+let or = x<y || z<y;
+let notEqual = x!=y;
+console.log(and , or , notEqual);
+
+//type conversion
+
+let num = 20; 
+let numToString = String(num);
+console.log(typeof num);
+console.log(typeof numToString);
+
+let str = "123";
+let strToNum = Number(str);
+console.log(typeof str);
+console.log(typeof strToNum);
+
+
